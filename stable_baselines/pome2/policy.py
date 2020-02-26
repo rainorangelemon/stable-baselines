8a8929ff404be5b901d7691bf8062628c9822b5b
rainorangelemon <rainorangelemon@gmail.com>
2020-02-25 21:57:12 -0800

    finetune

--- stable_baselines/pome2/policy.py
+++ stable_baselines/pome2/policy.py
@@ -101,9 +101,10 @@ class POMEPolicy(BasePolicy):
             layer_2 = activ(conv(layer_1, 'c4', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
             layer_de_2 = activ(deconv(layer_2, 'c5', n_filters=64, filter_size=4, stride=2, output_height=output_height, output_width=output_width, init_scale=np.sqrt(2), **kwargs))
             layer_de_1 = activ(deconv(layer_de_2, 'c6', n_filters=256, filter_size=4, stride=2, output_height=self.n_ob[0], output_width=self.n_ob[1], init_scale=np.sqrt(2), **kwargs))
-            layer_de_0 = tf.nn.softmax(layer_de_1, axis=-1)
+            layer_de_0 = activ(conv(layer_de_1, 'c7', n_filters=256, filter_size=3, stride=1, pad='SAME', init_scale=np.sqrt(2), **kwargs))
+            logits = tf.nn.softmax(layer_de_0, axis=-1)
 
-            next_frame = tf.reshape(layer_de_0, [-1, 256])
+            next_frame = tf.reshape(logits, [-1, 256])
             next_frame = tf.matmul(next_frame, tf.constant(np.arange(0, 256).reshape(256, 1).astype(np.float32)/255))
             next_frame = tf.reshape(next_frame, [-1, self.n_ob[0], self.n_ob[1]])
             rf_latent = conv_to_fc(layer_2)
